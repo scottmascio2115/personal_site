c43afdcceb0b3de3aace44eb2be8ce6115ea9169
Scott R. Mascio <ScottMascio@Scotts-MacBook-Pro.local>
2013-11-01 15:01:19 -0500

    first commit

---
+++ public/js/application.js
@@ -0,0 +1,165 @@
+$(document).ready(function() {
+
+  $("#about_trading").hide();
+  $("#trader").on('click', function(event){
+    event.preventDefault();
+  $("#about_husband").hide();
+  $("#about_webdeveloper").hide();
+  $("#about_adopt").hide();
+  $("#about_mudfish").hide();
+  $("#about_skills").hide();
+  $("#about_basketball").hide();
+  $("#about_ruby").hide();
+  $("#about_rspec").hide();
+  $("#about_testing").hide();
+  $('#about_trading').toggle();
+
+   });
+
+  $("#about_husband").hide();
+  $("#husband").on('click', function(event){
+    event.preventDefault();
+  $("#about_trading").hide();
+  $("#about_webdeveloper").hide();
+  $("#about_adopt").hide();
+  $("#about_mudfish").hide();
+  $("#about_skills").hide();
+  $("#about_basketball").hide();
+  $("#about_ruby").hide();
+  $("#about_rspec").hide();
+  $("#about_testing").hide();
+  $('#about_husband').toggle();
+
+   });
+
+  $("#about_webdeveloper").hide();
+  $("#web").on('click', function(event){
+    event.preventDefault();
+  $("#about_trading").hide();
+  $("#about_husband").hide();
+  $("#about_adopt").hide();
+  $("#about_mudfish").hide();
+  $("#about_skills").hide();
+  $("#about_basketball").hide();
+  $("#about_ruby").hide();
+  $("#about_rspec").hide();
+  $("#about_testing").hide();
+  $('#about_webdeveloper').toggle();
+
+   });
+
+  $("#about_adopt").hide();
+  $("#adopt").on('click', function(event){
+    event.preventDefault();
+  $("#about_trading").hide();
+  $("#about_husband").hide();
+  $("#about_webdeveloper").hide();
+  $("#about_mudfish").hide();
+  $("#about_skills").hide();
+  $("#about_basketball").hide();
+  $("#about_ruby").hide();
+  $("#about_rspec").hide();
+  $("#about_testing").hide();
+  $('#about_adopt').toggle();
+
+   });
+
+  $("#about_mudfish").hide();
+  $("#mudfish").on('click', function(event){
+    event.preventDefault();
+  $("#about_trading").hide();
+  $("#about_husband").hide();
+  $("#about_webdeveloper").hide();
+  $("#about_adopt").hide();
+  $("#about_skills").hide();
+  $("#about_basketball").hide();
+  $("#about_ruby").hide();
+  $("#about_rspec").hide();
+  $("#about_testing").hide();
+  $('#about_mudfish').toggle();
+
+   });
+
+  $("#about_skills").hide();
+  $("#skills").on('click', function(event){
+    event.preventDefault();
+  $("#about_trading").hide();
+  $("#about_husband").hide();
+  $("#about_webdeveloper").hide();
+  $("#about_adopt").hide();
+  $("#about_mudfish").hide();
+  $("#about_basketball").hide();
+  $("#about_ruby").hide();
+  $("#about_rspec").hide();
+  $("#about_testing").hide();
+  $('#about_skills').toggle();
+
+   });
+
+  $("#about_basketball").hide();
+  $("#basketball").on('click', function(event){
+    event.preventDefault();
+  $("#about_trading").hide();
+  $("#about_husband").hide();
+  $("#about_webdeveloper").hide();
+  $("#about_adopt").hide();
+  $("#about_mudfish").hide();
+  $("#about_skills").hide();
+  $("#about_ruby").hide();
+  $("#about_rspec").hide();
+  $("#about_testing").hide();
+  $('#about_basketball').toggle();
+
+   });
+
+  $("#about_ruby").hide();
+  $("#ruby").on('click', function(event){
+    event.preventDefault();
+  $("#about_trading").hide();
+  $("#about_husband").hide();
+  $("#about_webdeveloper").hide();
+  $("#about_adopt").hide();
+  $("#about_mudfish").hide();
+  $("#about_skills").hide();
+  $("#about_basketball").hide();
+  $("#about_rspec").hide();
+  $("#about_testing").hide();
+  $('#about_ruby').toggle();
+
+   });
+
+  $("#about_rspec").hide();
+  $("#rspec").on('click', function(event){
+    event.preventDefault();
+  $("#about_trading").hide();
+  $("#about_husband").hide();
+  $("#about_webdeveloper").hide();
+  $("#about_adopt").hide();
+  $("#about_mudfish").hide();
+  $("#about_skills").hide();
+  $("#about_basketball").hide();
+  $("#about_ruby").hide();
+  $("#about_testing").hide();
+  $('#about_rspec').toggle();
+
+   });
+
+  $("#about_testing").hide();
+  $("#testing").on('click', function(event){
+    event.preventDefault();
+  $("#about_trading").hide();
+  $("#about_husband").hide();
+  $("#about_webdeveloper").hide();
+  $("#about_adopt").hide();
+  $("#about_mudfish").hide();
+  $("#about_skills").hide();
+  $("#about_basketball").hide();
+  $("#about_ruby").hide();
+  $("#about_rspec").hide();
+  $('#about_testing').toggle();
+
+   });
+
+
+});
+
